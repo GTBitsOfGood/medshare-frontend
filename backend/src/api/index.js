02a5f560ee7a89925bd003011f211baf62d0df7f
Daniel Hsing <hsing.daniel@gmail.com>
2020-01-28 16:45:43 -0500

    chore: initialize backend boilerplate
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

---
+++ backend/src/api/index.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+
+// Health check API endpoint
+router.get("/", (req, res) => {
+    res.json({
+        message: "API is up and running!"
+    });
+});
+
+module.exports = router;
