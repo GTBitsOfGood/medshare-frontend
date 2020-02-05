08adf47d1eb965559afe2261504de0f72579fcfe
jlatour3 <jlatour3@gatech.edu>
2020-02-04 19:41:16 -0500

    Bielle and Jillian, first issue complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import {ThemeProvider} from "styled-components";
 
-function App() {
-    return <div></div>;
+const theme = {
+    fg: "palevioletred",
+    bg: "white"
+}  
+
+function App () {
+    const theme = ThemeProvider.div
+return <div>
+     <ThemeProvider theme={theme}> </ThemeProvider>
+    </div>;
 }
+
+
 
 export default App;
