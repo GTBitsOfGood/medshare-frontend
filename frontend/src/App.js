d19469c174bf89bbfc34fec871ea2bbcdf012b14
jlatour3 <jlatour3@gatech.edu>
2020-02-24 22:17:01 -0500

    Select Drop-Down, but the title will not change at top, haven't finished state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
-import React from "react";
-import {ThemeProvider} from "styled-components";
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Fallback from './components/Fallback';
 import Home from './components/Home';
 
 const theme = {
-    fg: "palevioletred",
-    bg: "white"
-}  
+  fg: 'palevioletred',
+  bg: 'white'
+};
 function App() {
   return (
     <Router>
       <ThemeProvider theme={theme}>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route component={Fallback} />
-      </Switch>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route component={Fallback} />
+        </Switch>
       </ThemeProvider>
     </Router>
   );
