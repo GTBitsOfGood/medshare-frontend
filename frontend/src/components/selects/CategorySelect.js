0c8a4db8868cbd47f4ca16c27988677fda9fd6f4
Kevin Wang <kw142143@gmail.com>
2020-03-03 19:20:34 -0500

    working button to deselect current category

--- frontend/src/components/selects/CategorySelect.js
+++ frontend/src/components/selects/CategorySelect.js
@@ -9,70 +9,53 @@ const SelectContainer = styled.div``;
 
 const categories = ['Medical Equipment', 'World Vision', 'Bio Med', 'Kendall'];
 
-let active = '';
-
-const SelectedItem = props => {
-  const { deselectFunc } = props;
-  return (
-    <div>
-      <button type="submit" onClick={deselectFunc}>
-        <img alt="deselect button" src={deselectButton} height="15" width="15" />
-      </button>
-    </div>
-  );
-};
-
-SelectedItem.propTypes = {
-  deselectFunc: PropTypes.func.isRequired
-};
-
-const itemListRenderer = ({ items, itemsParentRef, renderItem, activeItem }) => {
-  const renderedItems = items.map(renderItem).filter(item => item.props.text !== activeItem);
+const CategorySelect = ({ onSelect, category }) => {
   const deselectActive = () => {
-    active = '';
-    activeItem = active;
-    console.log('function??');
+    onSelect('');
   };
-  return (
-    <Menu ulRef={itemsParentRef}>
+  const SelectedItem = () => {
+    return (
+      <div>
+        <button type="submit" onClick={deselectActive}>
+          <img alt="deselect button" src={deselectButton} height="15" width="15" />
+        </button>
+      </div>
+    );
+  };
+
+  const itemListRenderer = ({ items, itemsParentRef, renderItem, activeItem }) => {
+    const renderedItems = items.map(renderItem).filter(item => item.props.text !== activeItem);
+    return (
+      <Menu ulRef={itemsParentRef}>
+        <MenuItem
+          key={activeItem}
+          text={activeItem}
+          labelElement={activeItem === '' ? null : <SelectedItem />}
+          shouldDismissPopover={false}
+        />
+        {renderedItems}
+      </Menu>
+    );
+  };
+
+  itemListRenderer.propTypes = {
+    items: PropTypes.shape.isRequired,
+    itemsParentRef: PropTypes.func.isRequired,
+    renderItem: PropTypes.func.isRequired,
+    activeItem: PropTypes.string.isRequired
+  };
+
+  const itemRenderer = (cate, { index, handleClick, modifiers }) => {
+    return (
       <MenuItem
-        key={activeItem}
-        text={activeItem}
-        labelElement={activeItem === '' ? null : <SelectedItem deselectFunc={deselectActive} />}
+        key={index + cate}
+        onClick={handleClick}
+        text={cate}
+        active={modifiers.active}
         shouldDismissPopover={false}
       />
-      {renderedItems}
-    </Menu>
-  );
-};
-
-itemListRenderer.propTypes = {
-  items: PropTypes.shape.isRequired,
-  itemsParentRef: PropTypes.func.isRequired,
-  renderItem: PropTypes.func.isRequired,
-  activeItem: PropTypes.string.isRequired
-};
-
-const itemRenderer = (category, { index, handleClick, modifiers }) => {
-  return (
-    <MenuItem
-      key={index + category}
-      onClick={handleClick}
-      text={category}
-      active={modifiers.active}
-      shouldDismissPopover={false}
-    />
-  );
-};
-
-const CategorySelect = ({ onSelect, category }) => {
-  onSelect(active);
-  const selectItem = item => {
-    onSelect(item);
-    active = item;
+    );
   };
-  console.log(active);
-  console.log('c: ' + category);
   return (
     <SelectContainer>
       <Select
@@ -82,7 +65,7 @@ const CategorySelect = ({ onSelect, category }) => {
         items={categories}
         itemListRenderer={itemListRenderer}
         itemRenderer={itemRenderer}
-        onItemSelect={selectItem}
+        onItemSelect={onSelect}
         activeItem={category}
       >
         <Button text={category || 'Category'} rightIcon="caret-down" />
