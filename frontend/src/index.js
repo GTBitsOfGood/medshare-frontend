4dba33f926ade8c6612cb04b0b1958e6d97c0e61
Daniel Hsing <hsing.daniel@gmail.com>
2020-03-05 08:49:02 -0500

    chore: add bold Ubuntu font, move font-family attribute to more specific selector
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/select/lib/css/blueprint-select.css';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
