7af4d003425d399a2ae774801391db1d1d917c48
jlatour3 <jlatour3@gatech.edu>
2020-03-31 20:19:42 -0400

    Nav Drawer added

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Alignment, Button, Navbar, NavbarGroup } from '@blueprintjs/core';
+import React, { useState } from 'react';
+import { Alignment, Button, Navbar, NavbarGroup, Drawer, Position } from '@blueprintjs/core';
 
 import styled from 'styled-components';
 import Logo from '../resources/medshare-logo.png';
@@ -12,11 +12,31 @@ const Wrapper = styled.div`
   padding: 0.5rem;
 `;
 
+const InsideDrawer = styled.div`
+  margin: 1rem;
+`;
+
 const NavBar = () => {
+  const [drawer, handleDrawer] = useState(false);
+
   return (
     <Navbar>
       <NavbarGroup align={Alignment.LEFT}>
-        <Button className="bp3-minimal" icon="menu" />
+        <Button className="bp3-minimal" icon="menu" onClick={() => handleDrawer(true)} />
+        <Drawer
+          background-color="#6396B3"
+          size="250px"
+          isOpen={drawer}
+          onClose={() => handleDrawer(false)}
+          position={Position.LEFT}
+        >
+          <InsideDrawer>
+            <Button classname="bp3-minimal" icon="menu" onClick={() => handleDrawer(false)} />
+            <h1>SEARCH </h1>
+            <h1>UPLOAD </h1>
+            <h1>SETTINGS </h1>
+          </InsideDrawer>
+        </Drawer>
       </NavbarGroup>
       <Wrapper>
         <img src={Logo} alt="medshare-logo" width="35rem" height="35rem" />
