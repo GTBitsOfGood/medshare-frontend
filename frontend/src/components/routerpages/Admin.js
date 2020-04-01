e9e47f4c5f413734817c637138f0e2b7d3d9cd3d
jlatour3 <jlatour3@gatech.edu>
2020-03-31 23:53:29 -0400

    Nav bar appears on all new pages

--- frontend/src/components/routerpages/Admin.js
+++ frontend/src/components/routerpages/Admin.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+import Header from '../Header';
+import { deviceSize } from '../../theme';
+
+const HeaderWrapper = styled.div`
+  @media ${deviceSize.mobileL} {
+    width: 100vw;
+  }
+  @media ${deviceSize.mobileS} {
+    width: 100vw;
+  }
+`;
 
 class Admin extends React.Component {
   render() {
-    return <span>ADMIN PORTAL</span>;
+    return (
+      <>
+        <HeaderWrapper>
+          <Header />
+        </HeaderWrapper>
+        <span>ADMIN PORTAL</span>
+      </>
+    );
   }
 }
 export default Admin;
--- frontend/src/components/routerpages/FAQ.js
+++ frontend/src/components/routerpages/FAQ.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+import Header from '../Header';
+import { deviceSize } from '../../theme';
+
+const HeaderWrapper = styled.div`
+  @media ${deviceSize.mobileL} {
+    width: 100vw;
+  }
+  @media ${deviceSize.mobileS} {
+    width: 100vw;
+  }
+`;
 
 class FAQ extends React.Component {
   render() {
-    return <h1>FAQ</h1>;
+    return (
+      <>
+        <HeaderWrapper>
+          <Header />
+        </HeaderWrapper>
+        <h1>FAQ</h1>
+      </>
+    );
   }
 }
 export default FAQ;
