7100cc3e244ba04ba97df4de8245a6c339287eff
Bernadette <bgbal@optonline.net>
2020-03-31 21:28:41 -0400

    Added router pages to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import { ThemeProvider } from 'styled-components';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Fallback from './components/routerpages/Fallback';
 import Search from './components/routerpages/Search';
+import FAQ from './components/routerpages/FAQ';
+import Admin from './components/routerpages/Admin';
+import Saved from './components/routerpages/Saved';
 import { theme } from './theme';
 
 function App() {
@@ -10,6 +13,18 @@ function App() {
     <Router>
       <ThemeProvider theme={theme}>
         <Switch>
+          <Route exact path="/">
+            <Search />
+          </Route>
+          <Route exact path="/faq">
+            <FAQ />
+          </Route>
+          <Route exact path="/admin">
+            <Admin />
+          </Route>
+          <Route exact path="/saved">
+            <Saved />
+          </Route>
           <Route exact path="/">
             <Search />
           </Route>
