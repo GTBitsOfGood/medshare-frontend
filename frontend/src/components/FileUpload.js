0c6bc0f1b6fdc7e19eeeee5b0c8d27f1bc065b0c
Kevin Wang <kw142143@gmail.com>
2020-04-10 16:41:04 -0400

    fixing browse link

--- frontend/src/components/FileUpload.js
+++ frontend/src/components/FileUpload.js
@@ -24,6 +24,7 @@ const Button = styled.button`
 const DropUpload = () => {
   const styles = { width: 704, height: 394 };
   const message = 'placeholder';
+  const fileInput = React.createRef();
   return (
     <div style={styles}>
       <FileDrop
@@ -34,12 +35,27 @@ const DropUpload = () => {
         onDragLeave={event => console.log('onDragLeave', event)}
         onDrop={(files, event) => console.log('onDrop!', files, event)}
       >
-        <input type="file" id="fileInput" />
+        <input type="file" id="fileInput" ref={fileInput} />
         Drag Document or{' '}
-        <button type="button" onClick={document.getElementById('fileInput').click()}>
+        <button
+          type="button"
+          id="fileInputButton"
+          onClick={() => {
+            fileInput.current.click();
+          }}
+        >
           Browse
         </button>
-        <svg width="60" height="73" viewBox="0 0 60 73" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg
+          onClick={() => {
+            fileInput.current.click();
+          }}
+          width="60"
+          height="73"
+          viewBox="0 0 60 73"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <path
             d="M37.3332 0.25H7.99984C3.9665 0.25 0.703171 3.5125 0.703171 7.5L0.666504 65.5C0.666504 69.4875 3.92984 72.75 7.96317 72.75H51.9998C56.0332 72.75 59.3332 69.4875 59.3332 65.5V22L37.3332 0.25ZM44.6665 51H33.6665V61.875H26.3332V51H15.3332V43.75H26.3332V32.875H33.6665V43.75H44.6665V51ZM33.6665 25.625V5.6875L53.8332 25.625H33.6665Z"
             fill="#6396B3"
